fceb97553bcee4796f844e30a6099c971a71e59c
Raphael Ventura <raphaelventura55@gmail.com>
2023-03-09 14:24:26 +0100

    sanity and frontend

---
+++ frontend/src/components/NavigationDots.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import {  } from "module";
+
+const NavigationDots = ({ active }) => (
+  <div className="app__navigation">
+    {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+      <a
+        href={`#${item}`}
+        key={item + index}
+        className="app__navigation-dot"
+        style={active === item ? { backgroundColor: '#313BAC' } : {}}
+      />
+    ))}
+  </div>
+);
+
+export default NavigationDots
+
+
+// const NavigationDots = ({ active }) => {
+//   return (
+//     <div className='app__navigation'>
+//       {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
+//         <a
+//           href={`#${item}`}
+//           key={item + index}
+//           className="app__navigation-dot"
+//           style={active === item ? { backgroundColor: '#313BAC' } : { }}
+//         />
+//       ))}
+//     </div>
+//   )}
